dc94f0db0e08956c1739316ee10004c839c98361
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-04 21:30:45 +0000

    Alle bisher erstellten Tests zusammengefasst in AllTests

---
+++ ToastWars/test/AllTests.java
@@ -0,0 +1,16 @@
+import com.user.AllUserTests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllTests extends TestCase
+{
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite("Alle Tests");
+		suite.addTest(AllUserTests.suite());
+		//TODO: addTestSuite(UserTest.class) and CoreTest.class
+		return suite;
+	}
+}
