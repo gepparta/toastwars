683290297431c59501cb4bd3306b2e9905e61c4d
michi.14@gmx.de <michi.14@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-05 13:28:50 +0000

    Anpassungen, um das Zeug über RPS

--- trunk/ToastWars/src/toastwars/server/datamodel/core/Type.java
+++ trunk/ToastWars/src/toastwars/server/datamodel/core/Type.java
@@ -1,6 +1,8 @@
 package toastwars.server.datamodel.core;
 
-public enum Type {
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public enum Type implements IsSerializable {
 	TYPE1("Der 1. Typ"), TYPE2("Der 2. Typ"), TYPE3("Der 3. Typ");
 
 	private String description;
