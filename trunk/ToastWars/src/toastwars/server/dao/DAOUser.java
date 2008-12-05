24623ce25a03292079d353631118b144a92dda83
tobiasmerx@gmx.de <tobiasmerx@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-05 11:59:44 +0000

    DBConnection erzeugt

--- trunk/ToastWars/src/toastwars/server/dao/DAOUser.java
+++ trunk/ToastWars/src/toastwars/server/dao/DAOUser.java
@@ -1,10 +1,33 @@
 package toastwars.server.dao;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DAOUser {
-public void createUser(String name, String password){
-String sql = "INSERT INTO tbl_user (name, password) VALUES ("+name+", "+password+");";
-}
-public void deleteUsers(){
-String sql= "DELETE FROM tbl_users WHERE ID <> 1;";
-}
+	public void createUser(String name, String password, String CompanyID,
+			Statement stmt) {
+		String sql = "INSERT INTO User (UserName, Password, CompanyID, Status)VALUES ('"
+				+ name
+				+ "','"
+				+ password
+				+ "','"
+				+ CompanyID
+				+ "', 'started');";
+		try {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteUsers(Statement stmt) {
+		String sql = "DELETE FROM User WHERE ID <> 1;";
+		try {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
