e8f4c49238f34e50ebaa25dc030b9f2645ad1597
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-06 00:45:22 +0000

    simulate zu 20% fertig

--- ToastWars/src/toastwars/server/datamodel/core/Toaster.java
+++ ToastWars/src/toastwars/server/datamodel/core/Toaster.java
@@ -2,24 +2,35 @@ package toastwars.server.datamodel.core;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class Toaster implements IsSerializable {
+public class Toaster implements IsSerializable
+{
 
 	private double price;
+
 	private double marketing;
+
 	private double research;
+
 	private double index;
+
 	private double turnover;
+
 	private double capital;
+
 	private double cost;
+
 	private double profit;
+
 	private int marketShare;
+
 	private Type type;
 
 	// //////////////////Konstruktor//////////////////////////
 
 	public Toaster(double price, double marketing, double research,
 			double index, double turnover, double capital, double cost,
-			double profit, int marketShare, Type type) {
+			double profit, int marketShare, Type type)
+	{
 
 		this.price = price;
 		this.marketing = marketing;
@@ -36,92 +47,137 @@ public class Toaster implements IsSerializable {
 
 	// /////////////////GETTER & SETTER ///////////////////////////////////
 
-	public double getPrice() {
+	public double getPrice()
+	{
 		return price;
 	}
 
-	public void setPrice(double price) {
+	public void setPrice(double price)
+	{
 		this.price = price;
 	}
 
-	public double getMarketing() {
+	public double getMarketing()
+	{
 		return marketing;
 	}
 
-	public void setMarketing(double marketing) {
+	public void setMarketing(double marketing)
+	{
 		this.marketing = marketing;
 	}
 
-	public double getResearch() {
+	public double getResearch()
+	{
 		return research;
 	}
 
-	public void setResearch(double research) {
+	public void setResearch(double research)
+	{
 		this.research = research;
 	}
 
-	public double getIndex() {
+	public double getIndex()
+	{
 		return index;
 	}
 
-	public void setIndex(double index) {
+	public void setIndex(double index)
+	{
 		this.index = index;
 	}
 
-	public double getTurnover() {
+	public double getTurnover()
+	{
 		return turnover;
 	}
 
-	public void setTurnover(double turnover) {
+	public void setTurnover(double turnover)
+	{
 		this.turnover = turnover;
 	}
 
-	public double getCapital() {
+	public double getCapital()
+	{
 		return capital;
 	}
 
-	public void setCapital(double capital) {
+	public void setCapital(double capital)
+	{
 		this.capital = capital;
 	}
 
-	public double getCost() {
+	public double getCost()
+	{
 		return cost;
 	}
 
-	public void setCost(double cost) {
+	public void setCost(double cost)
+	{
 		this.cost = cost;
 	}
 
-	public double getProfit() {
+	public double getProfit()
+	{
 		return profit;
 	}
 
-	public void setProfit(double profit) {
+	public void setProfit(double profit)
+	{
 		this.profit = profit;
 	}
 
-	public int getMarketShare() {
+	public int getMarketShare()
+	{
 		return marketShare;
 	}
 
-	public void setMarketShare(int marketShare) {
+	public void setMarketShare(int marketShare)
+	{
 		this.marketShare = marketShare;
 	}
 
-	public Type getType() {
+	public Type getType()
+	{
 		return type;
 	}
 
-	public void setType(Type type) {
+	public void setType(Type type)
+	{
 		this.type = type;
 	}
 
 	// ////////////METHODS ////////////////
 
-	public double calculateAndSetIndex(double random) {
+	public double calculateAndSetIndex(double random)
+	{
 		double index = research * (1 / price) * marketing * random;
 		this.setIndex(index);
 		return index;
+	}
+
+	// TODO: Wie Random setzen?
+	public void calculateIndex()
+	{
+		double marketingIndex = calculateMarketing();
+		double researchIndex = calculateResearch();
+		double random = 1.0;
+		double i = researchIndex * (1 / price) * marketingIndex * random;
+		setIndex(i);
+	}
+
+	public double calculateResearch()
+	{
+		double d = Math.log(this.research / 3000);
+		return Math.round(d * 100) / 100;
+	}
+
+	public double calculateMarketing()
+	{
+		double d = 6.49 + ((Math.pow(this.marketing / 10000 - 8, 3)
+				+ Math.pow(this.marketing / 10000 - 8, 2) + Math.pow(
+				this.marketing / 10000 - 8, 1)) / 80);
+		return Math.round(d * 100) / 100;
 	}
 
 }
