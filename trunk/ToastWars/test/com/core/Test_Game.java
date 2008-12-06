be33a6d7fc83c6ee1108de19d3001193cb53b9e5
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-06 00:45:38 +0000

    simulate zu 20% fertig

--- trunk/ToastWars/test/com/core/Test_Game.java
+++ trunk/ToastWars/test/com/core/Test_Game.java
@@ -1,25 +1,30 @@
 package com.core;
 
 import java.util.ArrayList;
-
+import static org.easymock.EasyMock.*;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import toastwars.server.datamodel.core.Company;
+import toastwars.server.datamodel.core.Game;
 import toastwars.server.datamodel.core.Toaster;
 import toastwars.server.datamodel.core.Type;
 
-public class Test_Game extends TestCase {
-
+public class Test_Game extends TestCase
+{
+	Game gameMock = createMock(Game.class);
+	
 	@Before
-	protected void setUp() throws Exception {
+	protected void setUp() throws Exception
+	{
 
 	}
 
 	@After
-	protected void tearDown() throws Exception {
+	protected void tearDown() throws Exception
+	{
 
 	}
 
