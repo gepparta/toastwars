117cd0dafd109553cf5d651c9dc920a29d99b882
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-07 13:46:10 +0000

    dem Type die Variable MarketVolume hinzugefügt

--- trunk/ToastWars/src/toastwars/server/datamodel/core/Type.java
+++ trunk/ToastWars/src/toastwars/server/datamodel/core/Type.java
@@ -3,15 +3,27 @@ package toastwars.server.datamodel.core;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public enum Type implements IsSerializable {
-	TYPE1("Der 1. Typ"), TYPE2("Der 2. Typ"), TYPE3("Der 3. Typ");
+	TYPE1("Der 1. Typ",10000), TYPE2("Der 2. Typ",20000), TYPE3("Der 3. Typ",30000);
 
 	private String description;
+	private int marketVolume;
 
-	Type(String description) {
+	Type(String description, int marketVolume) {
 		this.description = description;
+		this.marketVolume = marketVolume;
 	}
 
 	public String getDescription() {
 		return this.description;
+	}
+
+	public int getMarketVolume()
+	{
+		return marketVolume;
+	}
+
+	public void setMarketVolume(int marketVolume)
+	{
+		this.marketVolume = marketVolume;
 	}
 }
