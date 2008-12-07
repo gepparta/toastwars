6cace4673f15270e034d72f4b0a7b4bb1b9b2117
tobiasmerx@gmx.de <tobiasmerx@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-07 15:57:02 +0000

    Hab das ganze mal nach Waldis wünschen gestaltet. Allerdings muss noch die Company initialisert werden. Dafür brauch ich aber alle Tabellen was ich noch nicht hab :-)

--- trunk/ToastWars/src/toastwars/server/dao/DAOUser.java
+++ trunk/ToastWars/src/toastwars/server/dao/DAOUser.java
@@ -1,8 +1,14 @@
 package toastwars.server.dao;
 
-import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import toastwars.server.datamodel.core.Company;
+import toastwars.server.datamodel.user.Group;
+import toastwars.server.datamodel.user.Status;
 
 public class DAOUser {
 	public void createUser(String name, String password, String CompanyID) {
@@ -40,4 +46,41 @@ public class DAOUser {
 			e.printStackTrace();
 		}
 	}
+	public ArrayList getAllUsers() {
+        ArrayList columnNames = new ArrayList();
+        ArrayList data = new ArrayList();
+        try{
+            //Abfrage definieren
+            String query = "SELECT * FROM User;";
+    		DBConnection con = new DBConnection();
+    		con.connectToDB();
+    		Statement stmt = con.getStatement();
+            ResultSet rst = stmt.executeQuery(query);
+            ResultSetMetaData md = rst.getMetaData();
+            int columns = md.getColumnCount();
+            //  Zeileninhalt ermitteln
+            while (rst.next()) {
+                ArrayList <String>row = new ArrayList(columns);
+                for (int i = 1; i <= columns; i++) {
+                    row.add( rst.getString(i) );
+                }
+                //System.out.println(row.toString());
+                
+                Group obj = new Group(row.get(0), row.get(1));
+                Company comp = new Company();
+                obj.setCompany(comp);
+                Status stat = new Status(row.get(3));
+                obj.setStatus(stat);
+                data.add( obj);
+            }
+            System.out.println(data.toString());
+            rst.close();
+            stmt.close();
+			con.closeConnectionToDB();
+			return data;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
