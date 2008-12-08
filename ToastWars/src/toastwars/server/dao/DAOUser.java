bf326228d078cdbb34ce32e990eae43d9f09696f
tobiasmerx@gmx.de <tobiasmerx@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-08 08:50:19 +0000

    Habe die UserList als Klassenvariable deklariert und die demetsprechenden änderungen durchgeführt. Auch hab ich noch die Methode changeStatus geschrieben.

--- ToastWars/src/toastwars/server/dao/DAOUser.java
+++ ToastWars/src/toastwars/server/dao/DAOUser.java
@@ -11,7 +11,10 @@ import toastwars.server.datamodel.user.Group;
 import toastwars.server.datamodel.user.Status;
 
 public class DAOUser {
-	public void createUser(String name, String password, String CompanyID) {
+
+	private ArrayList userList;
+
+	public void createUser(String name, String password, Integer CompanyID) {
 		DBConnection con = new DBConnection();
 		con.connectToDB();
 		Statement stmt = con.getStatement();
@@ -41,46 +44,66 @@ public class DAOUser {
 			stmt.execute(sql);
 			stmt.close();
 			con.closeConnectionToDB();
+			userList.clear();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 	public ArrayList getAllUsers() {
-        ArrayList columnNames = new ArrayList();
-        ArrayList data = new ArrayList();
-        try{
-            //Abfrage definieren
-            String query = "SELECT * FROM User;";
-    		DBConnection con = new DBConnection();
-    		con.connectToDB();
-    		Statement stmt = con.getStatement();
-            ResultSet rst = stmt.executeQuery(query);
-            ResultSetMetaData md = rst.getMetaData();
-            int columns = md.getColumnCount();
-            //  Zeileninhalt ermitteln
-            while (rst.next()) {
-                ArrayList <String>row = new ArrayList(columns);
-                for (int i = 1; i <= columns; i++) {
-                    row.add( rst.getString(i) );
-                }
-                //System.out.println(row.toString());
-                
-                Group obj = new Group(row.get(0), row.get(1));
-                Company comp = new Company();
-                obj.setCompany(comp);
-                Status stat = new Status(row.get(3));
-                obj.setStatus(stat);
-                data.add( obj);
-            }
-            System.out.println(data.toString());
-            rst.close();
-            stmt.close();
+
+		if (userList.isEmpty()) {
+			try {
+				// Abfrage definieren
+				String query = "SELECT * FROM User;";
+				DBConnection con = new DBConnection();
+				con.connectToDB();
+				Statement stmt = con.getStatement();
+				ResultSet rst = stmt.executeQuery(query);
+				ResultSetMetaData md = rst.getMetaData();
+				int columns = md.getColumnCount();
+				// Zeileninhalt ermitteln
+				while (rst.next()) {
+					ArrayList<String> row = new ArrayList(columns);
+					for (int i = 1; i <= columns; i++) {
+						row.add(rst.getString(i));
+					}
+					// System.out.println(row.toString());
+
+					Group group = new Group(row.get(0), row.get(1));
+					Company comp = new Company();
+					group.setCompany(comp);
+					Status stat = new Status(row.get(3));
+					group.setStatus(stat);
+					userList.add(group);
+				}
+				System.out.println(userList.toString());
+				rst.close();
+				stmt.close();
+				con.closeConnectionToDB();
+				return userList;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		} else {
+			return userList;
+		}
+	}
+	public void changeStatus(String username, String status){
+		DBConnection con = new DBConnection();
+		con.connectToDB();
+		Statement stmt = con.getStatement();
+		String sql = "UPDATE [User] SET [User].Status = '" + status + "' WHERE (((User.UserName)='"+ username +"'));";
+		try {
+			stmt.execute(sql);
+			stmt.close();
 			con.closeConnectionToDB();
-			return data;
-        } catch(Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+			userList.clear();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
