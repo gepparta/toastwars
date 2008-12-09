62b2faac52657d28750d701626a39df264ac0963
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-09 20:46:47 +0000

    Wave Presentation added

---
+++ ToastWars/test/wavePresentation/PresentationWave1.java
@@ -0,0 +1,20 @@
+package wavePresentation;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+
+public class PresentationWave1 extends TestCase
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+}
