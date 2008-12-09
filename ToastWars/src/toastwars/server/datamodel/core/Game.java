3a64a8a1430657434f558ca6a0cc8bcc48abe2f0
waldemar.geppart <waldemar.geppart@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-09 11:05:48 +0000

    import des util packages angepasst

--- ToastWars/src/toastwars/server/datamodel/core/Game.java
+++ ToastWars/src/toastwars/server/datamodel/core/Game.java
@@ -1,7 +1,7 @@
 package toastwars.server.datamodel.core;
 
 import java.util.ArrayList;
-import util.NumberUtil;
+import toastwars.util.NumberUtil;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class Game implements IsSerializable {
