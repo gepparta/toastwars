ebb277cc1a42431ec4d4468f228f4113ea0b8af6
waldemar.geppart <waldemar.geppart@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-09 10:54:53 +0000

    roundDouble ohne NumberFormat

--- ToastWars/src/toastwars/util/NumberUtil.java
+++ ToastWars/src/toastwars/util/NumberUtil.java
@@ -1,15 +1,17 @@
 package toastwars.util;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
+public class NumberUtil {
+	// public static double roundDouble(double d) {
+	// DecimalFormat df = new DecimalFormat("0.00");
+	// df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.ENGLISH));
+	// return Double.parseDouble(df.format(d));
+	// }
 
-public class NumberUtil
-{
-	public static double roundDouble(double d)
-	{
-		DecimalFormat df = new DecimalFormat("0.00");
-		df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.ENGLISH));
-		return Double.parseDouble(df.format(d));
+	// by Waldi
+	public static double roundDouble(double d) {
+		int i = Math.round((float) (d * 100));
+		System.out.println(i);
+		return i / 100.00;
 	}
+
 }
