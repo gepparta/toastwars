783b54883b276b95aa1e8d99845b7d598532fd7e
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-10 15:54:01 +0000

    companyId hinzugefügt

--- ToastWars/src/toastwars/server/dao/DAOToaster.java
+++ ToastWars/src/toastwars/server/dao/DAOToaster.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.ArrayList;
-
 import toastwars.server.datamodel.core.Company;
 import toastwars.server.datamodel.core.Toaster;
 import toastwars.server.datamodel.core.Type;
@@ -12,12 +11,17 @@ import toastwars.server.datamodel.user.Group;
 import toastwars.server.datamodel.user.Status;
 import toastwars.server.datamodel.user.UserFactory;
 
-public class DAOToaster{
+public class DAOToaster
+{
 	ArrayList<Toaster> toasterList = new ArrayList<Toaster>();
-	public ArrayList getAllToaster() {
 
-		if (toasterList.isEmpty()) {
-			try {
+	public ArrayList getAllToaster()
+	{
+
+		if (toasterList.isEmpty())
+		{
+			try
+			{
 				// Abfrage definieren
 				String query = "SELECT * FROM Toaster;";
 				DBConnection con = new DBConnection();
@@ -27,34 +31,46 @@ public class DAOToaster{
 				ResultSetMetaData md = rst.getMetaData();
 				int columns = md.getColumnCount();
 				// Zeileninhalt ermitteln
-				while (rst.next()) {
-					ArrayList row = new ArrayList(columns);
-					for (int i = 1; i <= columns; i++) {
-						row.add(rst.getObject(i));
-					}
-					System.out.println(row.toString());
-					Toaster toaster = new Toaster(rst.getInt(1),rst.getDouble(3),rst.getDouble(4),rst.getDouble(5),rst.getDouble(6),rst.getDouble(7),rst.getDouble(8),rst.getDouble(9),rst.getInt(10),Type.valueOf(rst.getString(11)));
+				while (rst.next())
+				{
+					// ArrayList row = new ArrayList(columns);
+					// for (int i = 1; i <= columns; i++)
+					// {
+					// row.add(rst.getObject(i));
+					// System.out.print(i + ": " + md.getColumnName(i) + ": ");
+					// System.out.println(row.get(i - 1).toString());
+					// }
+					// System.out.println(row.toString());
+					Toaster toaster = new Toaster(rst.getInt(2), rst.getDouble(4),
+							rst.getDouble(5), rst.getDouble(6), rst.getDouble(7), rst.getDouble(8),
+							rst.getDouble(9), rst.getDouble(10), rst.getInt(11), Type.values()[rst
+									.getInt(12)]);
+					System.out.println();
 					System.out.println("test");
-					toasterList.add(toaster);
-					
+					// toasterList.add(toaster);
+
 				}
 				System.out.println(toasterList.toString());
 				rst.close();
 				stmt.close();
 				con.closeConnectionToDB();
 				return toasterList;
-			} catch (Exception e) {
+			} catch (Exception e)
+			{
 				e.printStackTrace();
 				return null;
 			}
-		} else {
+		} else
+		{
 			return toasterList;
 		}
 	}
-	public static void main(String[] args) {
+
+	public static void main(String[] args)
+	{
 		DAOToaster toaster = new DAOToaster();
-		//user.changeStatus("test","neuerStatus");
+		// user.changeStatus("test","neuerStatus");
 		toaster.getAllToaster();
 	}
-	
+
 }
--- ToastWars/test/wavePresentation/PresentationWave1.java
+++ ToastWars/test/wavePresentation/PresentationWave1.java
@@ -12,7 +12,6 @@ import toastwars.util.NumberUtil;
 
 public class PresentationWave1 extends TestCase
 {
-	private Type myType1;
 	private Toaster toaster1;
 	private Toaster toaster2;
 	private ArrayList<Toaster> toasterList1;
@@ -23,33 +22,15 @@ public class PresentationWave1 extends TestCase
 	@Before
 	public void setUp() throws Exception
 	{
-		myType1 = Type.TYPE1;
-		toaster1 = new Toaster(8, 1000, 8155, 1.00, 10000, 5000, 2.58, 500, myType1);
-		toaster2 = new Toaster(8, 1000, 8155, 1.00, 10000, 5000, 2.58, 500, myType1);
+		toaster1 = new Toaster(8, 1000, 8155, 1.00, 10000, 5000, 2.58, 500, Type.TYPE1);
+		toaster2 = new Toaster(8, 1000, 8155, 1.00, 10000, 5000, 2.58, 500, Type.TYPE1);
 	}
 
 	@After
 	public void tearDown() throws Exception
 	{
+		
 	}
 
-	@Test
-	public void test()
-	{
-		// double d = 0;
-		// do
-		// {
-		// d = NumberUtil.roundDouble(Math.random() * (1.05 - 0.95)) + 0.95;
-		// System.out.println(d);
-		// } while ((d >= 0.95) && (d <= 1.05));
-		// assertEquals("Falsch", 10, 8, 1);
-
-		double random = toaster1.getType().getRandom();
-		double d = 0;
-		do
-		{
-			d = NumberUtil.roundDouble(Math.random() * (random * 2)) + (1 - random);
-			System.out.println(d);
-		} while ((d >= 0.95) && (d <= 1.05));
-	}
+	
 }
