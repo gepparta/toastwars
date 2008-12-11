f06506e5a1f5c5ed2c08880cd7be598840734465
waldemar.geppart <waldemar.geppart@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-11 11:30:36 +0000

    Methoden zum Auf- und Abrunden hinzugefügt

--- ToastWars/src/toastwars/util/NumberUtil.java
+++ ToastWars/src/toastwars/util/NumberUtil.java
@@ -1,12 +1,30 @@
 package toastwars.util;
 
-public class NumberUtil
-{
+public class NumberUtil {
+
 	// by Waldi
-	public static double roundDouble(double d)
-	{
+	public static double roundDouble(double d) {
 		int i = Math.round((float) (d * 100));
 		return i / 100.00;
 	}
 
+	// by Waldi
+	public static int roundDoubleUp(double d, int stellen) {
+
+		if (d % stellen == 0)
+			return (int) d;
+
+		int i = Math.round((float) ((d + stellen / 2) / stellen));
+		return i * stellen;
+	}
+
+	// by Waldi
+	public static int roundDoubleDown(double d, int stellen) {
+
+		if (d % stellen == 0)
+			return (int) d;
+
+		int i = Math.round((float) ((d - stellen / 2) / stellen));
+		return i * stellen;
+	}
 }
