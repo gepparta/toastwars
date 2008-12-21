eba721f10a7a29c8283f9f30031f3b2084463ff2
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-21 22:40:18 +0000

    variableCost auf 5 € gesetzt

--- ToastWars/src/toastwars/server/datamodel/core/Type.java
+++ ToastWars/src/toastwars/server/datamodel/core/Type.java
@@ -2,10 +2,10 @@ package toastwars.server.datamodel.core;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public enum Type implements IsSerializable {
-	TYPE1("Der 1. Typ", 10000,0.05,1000.00,3.00,4000), 
-	TYPE2("Der 2. Typ", 20000,0.08,5000.00,15.00,1500), 
-	TYPE3("Der 3. Typ",30000,0.1,7500.00,60.00,500);
+public enum Type implements IsSerializable
+{
+	TYPE1("Der 1. Typ", 10000, 0.05, 1000.00, 5.00, 4000), TYPE2("Der 2. Typ", 20000, 0.08, 5000.00, 15.00, 1500), TYPE3("Der 3. Typ", 30000, 0.1,
+			7500.00, 60.00, 500);
 
 	private String description;
 	private int marketVolume;
@@ -14,10 +14,8 @@ public enum Type implements IsSerializable {
 	private double variableCost;
 	private int capacity;
 
-	
-
-
-	Type(String description, int marketVolume, double random, double fixCost, double variableCost, int capacity) {
+	Type(String description, int marketVolume, double random, double fixCost, double variableCost, int capacity)
+	{
 		this.description = description;
 		this.marketVolume = marketVolume;
 		this.random = random;
@@ -25,13 +23,14 @@ public enum Type implements IsSerializable {
 		this.variableCost = variableCost;
 		this.capacity = capacity;
 	}
-	
+
 	public int getCapacity()
 	{
 		return capacity;
 	}
 
-	public String getDescription() {
+	public String getDescription()
+	{
 		return this.description;
 	}
 
@@ -40,26 +39,28 @@ public enum Type implements IsSerializable {
 		return fixCostPerMachine;
 	}
 
-	public int getMarketVolume() {
+	public int getMarketVolume()
+	{
 		return marketVolume;
 	}
 
-	public double getRandom() {
+	public double getRandom()
+	{
 		return random;
 	}
 
-	public double getVariableCost() {
+	public double getVariableCost()
+	{
 		return variableCost;
 	}
-
-
 
 	public void setCapacity(int capacity)
 	{
 		this.capacity = capacity;
 	}
 
-	public void setDescription(String description) {
+	public void setDescription(String description)
+	{
 		this.description = description;
 	}
 
@@ -68,16 +69,18 @@ public enum Type implements IsSerializable {
 		this.fixCostPerMachine = fixCostPerMachine;
 	}
 
-	public void setMarketVolume(int marketVolume) {
+	public void setMarketVolume(int marketVolume)
+	{
 		this.marketVolume = marketVolume;
 	}
 
-	public void setRandom(double random) {
+	public void setRandom(double random)
+	{
 		this.random = random;
 	}
 
-	public void setVariableCost(double variableCost) {
+	public void setVariableCost(double variableCost)
+	{
 		this.variableCost = variableCost;
 	}
-
 }
