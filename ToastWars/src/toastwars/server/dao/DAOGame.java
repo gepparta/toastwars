c8301c0cb66267766141df25d41cc4c64acf6e8c
tobiasmerx@gmx.de <tobiasmerx@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-27 14:33:37 +0000

    Methode createInitialData erstellt. Allerdings noch mit Fantasiewerten. P.S: Nehmt mal endlich die Description aus der Company raus :-P

--- ToastWars/src/toastwars/server/dao/DAOGame.java
+++ ToastWars/src/toastwars/server/dao/DAOGame.java
@@ -14,7 +14,7 @@ import toastwars.server.datamodel.user.UserFactory;
 
 public class DAOGame {
 
-	public ArrayList<Group> getAllUsers(){
+	public ArrayList<Group> getAllUsers() {
 		try {
 			DBConnection con = new DBConnection();
 			con.connectToDB();
@@ -28,7 +28,8 @@ public class DAOGame {
 			return null;
 		}
 	}
-	public Integer getCurrentRound(){
+
+	public Integer getCurrentRound() {
 		try {
 			String query = "SELECT Game.[CurrentRound]FROM Game;";
 			DBConnection con = new DBConnection();
@@ -36,19 +37,20 @@ public class DAOGame {
 			Statement stmt = con.getStatement();
 			ResultSet rst = stmt.executeQuery(query);
 			rst.next();
-			Integer currentRound=rst.getInt(1);
+			Integer currentRound = rst.getInt(1);
 			rst.close();
 			stmt.close();
 			con.closeConnectionToDB();
 			return currentRound;
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 	}
-	public Integer getUserAmount(){
+
+	public Integer getUserAmount() {
 		try {
 			String query = "SELECT Game.[NumberOfUsers]FROM Game;";
 			DBConnection con = new DBConnection();
@@ -56,7 +58,7 @@ public class DAOGame {
 			Statement stmt = con.getStatement();
 			ResultSet rst = stmt.executeQuery(query);
 			rst.next();
-			Integer UserAmount=rst.getInt(1);
+			Integer UserAmount = rst.getInt(1);
 			rst.close();
 			stmt.close();
 			con.closeConnectionToDB();
@@ -67,26 +69,55 @@ public class DAOGame {
 			return null;
 		}
 	}
+
+	public void createInitialData(Integer userAmount) {
+		try {
+			DBConnection con = new DBConnection();
+			con.connectToDB();
+			Statement stmt = con.getStatement();
+			String query = "INSERT INTO Game VALUES (0," + userAmount
+					+ ", 'Instruction');";
+			stmt.execute(query);
+			for (int i = 1; i <= userAmount; i++) {
+				query = "INSERT INTO User VALUES ('user" + i + "','password"
+						+ i + "'," + i + ",'CREATED');";
+				stmt.execute(query);
+				query = "INSERT INTO Company VALUES (0," + i + ",'test',1000,1000,1000,1000,1000);";
+				stmt.execute(query);
+				query = "INSERT INTO Toaster VALUES (0," + i + "," + i + ",1000,1000,1000," +
+						"1000,1000,1000,1000,1000,'TYPE1');";
+				stmt.execute(query);
+			}
+			stmt.close();
+			con.closeConnectionToDB();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		DAOGame user = new DAOGame();
-		ArrayList<Group> userList = user.getAllUsers();
-		Group test= userList.get(0);
-		System.out.println(test.getUsername());
-		Company test1=test.getCompany();
-		System.out.println(test1.getCompanyID());
-		ArrayList<Toaster> test6 = test1.getToasterList();
-		System.out.println(test6.get(0).getToasterID());
-		System.out.println(test6.get(1).getToasterID());
-
-		
-		Group test2=userList.get(1);
-		System.out.println(test2.getUsername());
-		Company test3=test2.getCompany();
-		System.out.println(test3.getCompanyID());
-		ArrayList<Toaster> test7 = test3.getToasterList();
-		System.out.println(test7.get(0).getToasterID());
-		System.out.println(test7.get(1).getToasterID());
-		System.out.println(user.getCurrentRound());
-		System.out.println(user.getUserAmount());
+		// ArrayList<Group> userList = user.getAllUsers();
+		// Group test= userList.get(0);
+		// System.out.println(test.getUsername());
+		// Company test1=test.getCompany();
+		// System.out.println(test1.getCompanyID());
+		// ArrayList<Toaster> test6 = test1.getToasterList();
+		// System.out.println(test6.get(0).getToasterID());
+		// System.out.println(test6.get(1).getToasterID());
+		//
+		//		
+		// Group test2=userList.get(1);
+		// System.out.println(test2.getUsername());
+		// Company test3=test2.getCompany();
+		// System.out.println(test3.getCompanyID());
+		// ArrayList<Toaster> test7 = test3.getToasterList();
+		// System.out.println(test7.get(0).getToasterID());
+		// System.out.println(test7.get(1).getToasterID());
+		// System.out.println(user.getCurrentRound());
+		// System.out.println(user.getUserAmount());
+		user.createInitialData(4);
 	}
 }
