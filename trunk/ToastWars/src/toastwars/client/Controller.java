24f016869070ee34c531ed01ecb82d72ef09ea29
waldemar.geppart <waldemar.geppart@19d0b848-b977-11dd-a067-9b1d96bc3259>
2008-12-28 19:54:50 +0000

    Umstrukturierung der UI-Klassen

--- trunk/ToastWars/src/toastwars/client/Controller.java
+++ trunk/ToastWars/src/toastwars/client/Controller.java
@@ -1,14 +1,33 @@
 package toastwars.client;
 
+import toastwars.client.ui.LoginWindow;
+import toastwars.server.datamodel.user.Group;
 import toastwars.server.datamodel.user.IUser;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class Controller {
 
-	private ToastWars	toastWars;
+	private static Controller	controller;
+	private LoginWindow			loginWindow;
+	private IUser				user;
 
-	public void login(String name, String pwd, ToastWars tw) {
-		toastWars = tw;
+	// Benutzer-Parameter
+	public static final int		SPIELLEITER	= 1;
+	public static final int		GRUPPE		= 2;
+	private int					userType;
+
+	private Controller() {
+	}
+
+	public static Controller getInstance() {
+		if (controller == null)
+			controller = new Controller();
+
+		return controller;
+	}
+
+	public void login(String name, String pwd, LoginWindow loginW) {
+		loginWindow = loginW;
 		ToastWarsServiceAsync service = ToastWarsService.Util.getInstance();
 
 		AsyncCallback<IUser> callback = new AsyncCallback<IUser>() {
@@ -16,10 +35,39 @@ public class Controller {
 			}
 
 			public void onSuccess(IUser result) {
-				toastWars.login(result);
+				user = result;
+				loginWindow.login(result);
 			}
 		};
 
 		service.login(name, pwd, callback);
+	}
+
+	public void save() {
+		ToastWarsServiceAsync service = ToastWarsService.Util.getInstance();
+
+		AsyncCallback callback = new AsyncCallback() {
+			public void onFailure(Throwable caught) {
+			}
+
+			public void onSuccess(Object result) {
+			}
+		};
+
+		((Group) user).getCompany().getToasterList().get(0).setPrice(5);
+
+		service.save((Group) user, callback);
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+
+	public IUser getUser() {
+		return user;
 	}
 }
---
+++ trunk/ToastWars/src/toastwars/client/ui/LoginWindow.java
@@ -0,0 +1,80 @@
+package toastwars.client.ui;
+
+import toastwars.client.Controller;
+import toastwars.server.datamodel.user.Group;
+import toastwars.server.datamodel.user.IUser;
+import toastwars.server.datamodel.user.Master;
+
+import com.gwtext.client.core.EventObject;
+import com.gwtext.client.widgets.Button;
+import com.gwtext.client.widgets.MessageBox;
+import com.gwtext.client.widgets.Window;
+import com.gwtext.client.widgets.event.ButtonListenerAdapter;
+import com.gwtext.client.widgets.form.FormPanel;
+import com.gwtext.client.widgets.form.TextField;
+
+public class LoginWindow extends Window {
+
+	private LoginWindow	loginWindow;
+	private TextField	userName;
+	private TextField	userPass;
+	private IUser		user;
+	private ToastWars	toastWars;
+	private Controller	controller;
+
+	public LoginWindow(ToastWars tw) {
+
+		loginWindow = this;
+		toastWars = tw;
+		controller = Controller.getInstance();
+
+		setTitle("Anmeldung");
+		setSize(300, 150);
+		setClosable(false);
+		setDraggable(false);
+		setModal(true);
+		setResizable(false);
+
+		FormPanel loginPanel = createLoginPanel();
+
+		add(loginPanel);
+		show();
+	}
+
+	private FormPanel createLoginPanel() {
+		FormPanel loginPanel = new FormPanel();
+		loginPanel.setPaddings(10);
+
+		userName = new TextField("Benutzer");
+		userName.setValue("gruppe");
+		userPass = new TextField("Kennwort");
+		userPass.setValue("master");
+		userPass.setPassword(true);
+
+		loginPanel.add(userName);
+		loginPanel.add(userPass);
+
+		loginPanel.addButton(new Button("Anmelden",
+				new ButtonListenerAdapter() {
+					public void onClick(Button button, EventObject e) {
+						// User und Passwort checken
+						Controller.getInstance().login(userName.getText(),
+								userPass.getText(), loginWindow);
+					}
+				}));
+		return loginPanel;
+	}
+
+	public void login(IUser user) {
+		if (user != null) {
+			this.user = user;
+			if (user instanceof Master)
+				controller.setUserType(Controller.SPIELLEITER);
+			else if (user instanceof Group)
+				controller.setUserType(Controller.GRUPPE);
+			loginWindow.close();
+			toastWars.createUI();
+		} else
+			MessageBox.alert("Anmeldung fehlgeschlagen!");
+	}
+}
