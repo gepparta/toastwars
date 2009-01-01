d97bc2d87210547d5430569371cac5fc6f386fee
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-01 14:52:06 +0000

    new attributes for marketing and research added

--- ToastWars/src/toastwars/server/datamodel/core/Toaster.java
+++ ToastWars/src/toastwars/server/datamodel/core/Toaster.java
@@ -9,28 +9,42 @@ public class Toaster implements IsSerializable
 	private static int nextToasterID = 1;
 	private int toasterID;
 	private double price;
-	private double marketing;
-	private double research;
 	private double index;
 	private double turnover;
 	private double cost;
 	private double profit;
 	private int marketShare;
 	private Type type;
-	
-	public Toaster() {
+//	Faktoren f¸r marketing**************************************
+	private double marketing;
+	private double tvInvestment;
+	private double newsPaperInvestment;
+	private double radioInvestment;
+//	Faktoren f¸r  research**************************************
+	private double research;
+	private double quality;
+	private double design;
+	private double efficiency;
+
+	public Toaster()
+	{
 	}
-	
-	public Toaster(double price, double marketing, double research,
-			double index, double turnover, double cost, double profit,
-			int marketShare, Type type)
+
+	public Toaster(double price, double marketing, double tvInvestment, double newsPaperInvestment, double radioInvestment, double research,
+			double quality, double design, double efficiency, double index, double turnover, double cost, double profit, int marketShare, Type type)
 	{
 
 		this.toasterID = nextToasterID;
 		nextToasterID++;
 		this.price = price;
 		this.marketing = marketing;
+		this.tvInvestment = tvInvestment;
+		this.newsPaperInvestment = newsPaperInvestment;
+		this.radioInvestment = radioInvestment;
 		this.research = research;
+		this.quality = quality;
+		this.design = design;
+		this.efficiency = efficiency;
 		this.index = index;
 		this.turnover = turnover;
 		this.cost = cost;
@@ -39,14 +53,21 @@ public class Toaster implements IsSerializable
 		this.type = type;
 
 	}
-	public Toaster(int toasterID, double price, double marketing,
-			double research, double index, double turnover, double cost,
-			double profit, int marketShare, Type type)
+
+	public Toaster(int toasterID, double price, double marketing, double tvInvestment, double newsPaperInvestment, double radioInvestment,
+			double research, double quality, double design, double efficiency, double index, double turnover, double cost, double profit,
+			int marketShare, Type type)
 	{
 		this.toasterID = toasterID;
 		this.price = price;
 		this.marketing = marketing;
+		this.tvInvestment = tvInvestment;
+		this.newsPaperInvestment = newsPaperInvestment;
+		this.radioInvestment = radioInvestment;
 		this.research = research;
+		this.quality = quality;
+		this.design = design;
+		this.efficiency = efficiency;
 		this.index = index;
 		this.turnover = turnover;
 		this.cost = cost;
@@ -54,7 +75,6 @@ public class Toaster implements IsSerializable
 		this.marketShare = marketShare;
 		this.type = type;
 	}
-
 
 	public static int getNextToasterID()
 	{
@@ -69,12 +89,12 @@ public class Toaster implements IsSerializable
 	public void calculateCost()
 	{
 		this.setCost((this.getMarketShare() * this.type.getVariableCost())
-					+ (this.type.getFixCostPerMachine() 
-					* NumberUtil.roundDoubleUp((double) this.getMarketShare(),this.type.getCapacity())/this.type.getCapacity()));
+				+ (this.type.getFixCostPerMachine() * NumberUtil.roundDoubleUp((double) this.getMarketShare(), this.type.getCapacity()) / this.type
+						.getCapacity()));
 	}
 
 	// @by Alex
-	// Das Random fÔøΩr den jeweiligen ToasterTyp mit den verschiedenen
+	// Das Random f¸r den jeweiligen ToasterTyp mit den verschiedenen
 	// Abweichungen:
 	// Type1 : 5% --> wird im Enum Konstruktor definiert
 	// Type2 : 8%
@@ -85,8 +105,7 @@ public class Toaster implements IsSerializable
 		double marketingIndex = calculateMarketing();
 		double researchIndex = calculateResearch();
 		double random = this.getType().getRandom();
-		double i = researchIndex * marketingIndex
-				  *	(1 / Math.pow((price / 10), 2.5));			
+		double i = researchIndex * marketingIndex * (1 / Math.pow((price / 10), 2.5));
 //				  * NumberUtil.roundDouble(Math.random() * (random * 2))+ (1 - random);
 		// runden auf zwei Stellen hinter dem Komma
 		setIndex(NumberUtil.roundDouble(i));
@@ -98,8 +117,7 @@ public class Toaster implements IsSerializable
 		double marketingIndex = calculateMarketing();
 		double researchIndex = calculateResearch();
 		double random = this.getType().getRandom();
-		double i = researchIndex * marketingIndex
-				  *	(1 / Math.pow((price / 10), 2.5));			
+		double i = researchIndex * marketingIndex * (1 / Math.pow((price / 10), 2.5));
 //				  * NumberUtil.roundDouble(Math.random() * (random * 2))+ (1 - random);
 		// runden auf zwei Stellen hinter dem Komma
 		setIndex(NumberUtil.roundDouble(i));
@@ -108,18 +126,17 @@ public class Toaster implements IsSerializable
 	// @by Alex
 	public double calculateMarketing()
 	{
+		this.marketing = this.tvInvestment + this.newsPaperInvestment + this.radioInvestment;
 //		4.67 ==> Startwert 10.000Ä damit Index =1
-		double d = 4.7625 + ((Math.pow(this.marketing / 10000 - 8, 3)
-				         +  Math.pow(this.marketing / 10000 - 8, 2) 
-				         +  Math.pow(this.marketing / 10000 - 8, 1))/80);
+		double d = 4.7625 + ((Math.pow(this.marketing / 10000 - 8, 3) + Math.pow(this.marketing / 10000 - 8, 2) + Math.pow(
+				this.marketing / 10000 - 8, 1)) / 80);
 		return NumberUtil.roundDouble(d);
 	}
 
 	public void calculateMarketShare(double IndexSum)
 	{
 
-		this.setMarketShare((int) Math.round(this.type.getMarketVolume()
-				/ IndexSum * this.index));
+		this.setMarketShare((int) Math.round(this.type.getMarketVolume() / IndexSum * this.index));
 
 	}
 
@@ -131,8 +148,8 @@ public class Toaster implements IsSerializable
 	// @by Alex
 	public double calculateResearch()
 	{
-		double d = Math.log(this.research / 3000)
-				* (this.research / (Math.pow(this.research, 1.04)));
+		this.research = this.quality + this.design + this.efficiency;
+		double d = Math.log(this.research / 3000) * (this.research / (Math.pow(this.research, 1.04)));
 		return NumberUtil.roundDouble(d);
 	}
 
@@ -216,23 +233,23 @@ public class Toaster implements IsSerializable
 
 	public void setPrice(double price) throws Exception
 	{
-		if(this.type == Type.TYPE1)
+		if (this.type == Type.TYPE1)
 		{
-			if(price<=20 && price>=5)
+			if (price <= 20 && price >= 5)
 				this.price = price;
 			else
 				throw new Exception("Ung√ºltiger Preis");
 		}
-		if(this.type == Type.TYPE2)
+		if (this.type == Type.TYPE2)
 		{
-			if(price<=60 && price>=30)
+			if (price <= 60 && price >= 30)
 				this.price = price;
 			else
 				throw new Exception("Ung√ºltiger Preis");
 		}
-		if(this.type == Type.TYPE3)
+		if (this.type == Type.TYPE3)
 		{
-			if(price<=200 && price>=130)
+			if (price <= 200 && price >= 130)
 				this.price = price;
 			else
 				throw new Exception("Ung√ºltiger Preis");
@@ -262,6 +279,66 @@ public class Toaster implements IsSerializable
 	public void setType(Type type)
 	{
 		this.type = type;
+	}
+
+	public double getTvInvestment()
+	{
+		return tvInvestment;
+	}
+
+	public double getNewsPaperInvestment()
+	{
+		return newsPaperInvestment;
+	}
+
+	public double getRadioInvestment()
+	{
+		return radioInvestment;
+	}
+
+	public double getQuality()
+	{
+		return quality;
+	}
+
+	public double getDesign()
+	{
+		return design;
+	}
+
+	public double getEfficiency()
+	{
+		return efficiency;
+	}
+
+	public void setTvInvestment(double tvInvestment)
+	{
+		this.tvInvestment = tvInvestment;
+	}
+
+	public void setNewsPaperInvestment(double newsPaperInvestment)
+	{
+		this.newsPaperInvestment = newsPaperInvestment;
+	}
+
+	public void setRadioInvestment(double radioInvestment)
+	{
+		this.radioInvestment = radioInvestment;
+	}
+
+	public void setQuality(double quality)
+	{
+		this.quality = quality;
+	}
+
+	public void setDesign(double design)
+	{
+		this.design = design;
+	}
+
+	public void setEfficiency(double efficiency)
+	{
+		this.efficiency = efficiency;
 	}
 
 }
