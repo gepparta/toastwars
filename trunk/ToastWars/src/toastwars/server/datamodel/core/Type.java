b9a4cd72944ce567d775683e44bab74915edff0d
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-01 19:23:10 +0000

    auf neue funktionen umgestellt

--- trunk/ToastWars/src/toastwars/server/datamodel/core/Type.java
+++ trunk/ToastWars/src/toastwars/server/datamodel/core/Type.java
@@ -4,22 +4,25 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 
 public enum Type implements IsSerializable
 {
-	TYPE1("Der 1. Typ", 10000, 0.05, 1000.00, 5.00, 4000), TYPE2("Der 2. Typ", 20000, 0.08, 5000.00, 15.00, 1500), TYPE3("Der 3. Typ", 30000, 0.1,
-			7500.00, 60.00, 500);
+	TYPE1("Der 1. Typ", 10000, 0.05, 10000.00, 1000.00, 3.00, 4000), 
+	TYPE2("Der 2. Typ", 6000, 0.08, 20000.00, 5000.00, 15.00, 1500), 
+	TYPE3("Der 3. Typ", 2500, 0.10, 25000.00, 8000.00, 40.00, 500);
 
 	private String description;
 	private int marketVolume;
 	private double random;
-	private double fixCostPerMachine;
+	private double fixCost;
+	private double stepCosts;
 	private double variableCost;
 	private int capacity;
 
-	Type(String description, int marketVolume, double random, double fixCost, double variableCost, int capacity)
+	Type(String description, int marketVolume, double random, double fixCost, double stepCosts, double variableCost, int capacity)
 	{
 		this.description = description;
 		this.marketVolume = marketVolume;
 		this.random = random;
-		this.fixCostPerMachine = fixCost;
+		this.fixCost = fixCost;
+		this.stepCosts = stepCosts;
 		this.variableCost = variableCost;
 		this.capacity = capacity;
 	}
@@ -36,7 +39,7 @@ public enum Type implements IsSerializable
 
 	public double getFixCostPerMachine()
 	{
-		return fixCostPerMachine;
+		return fixCost;
 	}
 
 	public int getMarketVolume()
@@ -47,6 +50,11 @@ public enum Type implements IsSerializable
 	public double getRandom()
 	{
 		return random;
+	}
+
+	public double getStepCosts()
+	{
+		return stepCosts;
 	}
 
 	public double getVariableCost()
@@ -66,7 +74,7 @@ public enum Type implements IsSerializable
 
 	public void setFixCostPerMachine(double fixCostPerMachine)
 	{
-		this.fixCostPerMachine = fixCostPerMachine;
+		this.fixCost = fixCostPerMachine;
 	}
 
 	public void setMarketVolume(int marketVolume)
@@ -82,5 +90,10 @@ public enum Type implements IsSerializable
 	public void setVariableCost(double variableCost)
 	{
 		this.variableCost = variableCost;
+	}
+
+	public void setStepCosts(double stepCosts)
+	{
+		this.stepCosts = stepCosts;
 	}
 }
