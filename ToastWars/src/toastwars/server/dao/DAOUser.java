f8c6f2b6601c4cd361259ef1d413411ce15c6f7e
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-09 21:11:04 +0000

    method findUser added

--- ToastWars/src/toastwars/server/dao/DAOUser.java
+++ ToastWars/src/toastwars/server/dao/DAOUser.java
@@ -12,92 +12,124 @@ import toastwars.server.datamodel.user.IUser;
 import toastwars.server.datamodel.user.Status;
 import toastwars.server.datamodel.user.UserFactory;
 
-public class DAOUser {
+public class DAOUser
+{
 
 	private ArrayList<IUser> userList = new ArrayList<IUser>();
 
-	public void saveUser(String name, String password, Integer CompanyID) {
+	public void saveUser(String name, String password, Integer CompanyID)
+	{
 		DBConnection con = new DBConnection();
 		con.connectToDB();
 		Statement stmt = con.getStatement();
-		String sql = "INSERT INTO User (UserName, Password, CompanyID, Status)VALUES ('"
-				+ name
-				+ "','"
-				+ password
-				+ "','"
-				+ CompanyID
-				+ "', 'started');";
-		try {
+		String sql = "INSERT INTO User (UserName, Password, CompanyID, Status)VALUES ('" + name + "','" + password + "','" + CompanyID + "', 'started');";
+		try
+		{
 			stmt.execute(sql);
 			stmt.close();
 			con.closeConnectionToDB();
-		} catch (SQLException e) {
+		} catch (SQLException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public void deleteUsers() {
+	public void deleteUsers()
+	{
 		DBConnection con = new DBConnection();
 		con.connectToDB();
 		Statement stmt = con.getStatement();
 		String sql = "DELETE * FROM User;";
-		try {
+		try
+		{
 			stmt.execute(sql);
 			stmt.close();
 			con.closeConnectionToDB();
 			userList.clear();
-		} catch (SQLException e) {
+		} catch (SQLException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public ArrayList<IUser> getAllUsers(DBConnection con) {
-			try {
-				userList.clear();
-				// Abfrage definieren
-				String query = "SELECT * FROM User;";
-				Statement stmt = con.getStatement();
-				ResultSet rst = stmt.executeQuery(query);
-				DAOCompany test = new DAOCompany();
-				ArrayList<Company> companyList = test.getAllCurrentCompanies(con);
-				Integer size =companyList.size();
-				// Zeileninhalt ermitteln
-				Integer counter= 0;
-				while (rst.next()) {
-					Group group = (Group)UserFactory.createUser("Group",rst.getString(1), rst.getString(2));
-					group.setCompany(companyList.get(counter));
-					Status stat = Status.valueOf(rst.getString(4));
-					group.setStatus(stat);
-					userList.add(group);
-					counter++;
-				}
-				rst.close();
-				stmt.close();
-				return userList;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return null;
+
+	public ArrayList<IUser> getAllUsers()
+	{
+		ArrayList<IUser> tempUserList = new ArrayList<IUser>();
+		DBConnection con = new DBConnection();
+		con.connectToDB();
+		try
+		{
+			tempUserList.clear();
+			// Abfrage definieren
+			String query = "SELECT * FROM User;";
+			Statement stmt = con.getStatement();
+			ResultSet rst = stmt.executeQuery(query);
+			DAOCompany test = new DAOCompany();
+			ArrayList<Company> companyList = test.getAllCurrentCompanies(con);
+			Integer size = companyList.size();
+			// Zeileninhalt ermitteln
+			Integer counter = 0;
+			while (rst.next())
+			{
+				Group group = (Group) UserFactory.createUser("Group", rst.getString(1), rst.getString(2));
+				group.setCompany(companyList.get(counter));
+				Status stat = Status.valueOf(rst.getString(4));
+				group.setStatus(stat);
+				tempUserList.add(group);
+				counter++;
 			}
+			rst.close();
+			stmt.close();
+			return tempUserList;
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
 	}
-	public void changeStatus(String username, String status){
+
+	public void changeStatus(String username, String status)
+	{
 		DBConnection con = new DBConnection();
 		con.connectToDB();
 		Statement stmt = con.getStatement();
-		String sql = "UPDATE [User] SET [User].Status = '" + status + "' WHERE (((User.UserName)='"+ username +"'));";
-		try {
+		String sql = "UPDATE [User] SET [User].Status = '" + status + "' WHERE (((User.UserName)='" + username + "'));";
+		try
+		{
 			stmt.execute(sql);
 			stmt.close();
 			con.closeConnectionToDB();
 			userList.clear();
-		} catch (SQLException e) {
+		} catch (SQLException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public static void main(String[] args) {
+
+	public IUser findUser(String name, String pass) throws Exception
+	{
+		if (userList.size() == 0)
+		{
+			userList = this.getAllUsers();
+		} 
+		
+		for(int i=0;i<=userList.size();i++)
+		{
+			if(userList.get(i).getUsername().equals(name))
+				if(userList.get(i).getPassword().equals(pass))
+			return 	userList.get(i);	
+		}
+			
+		throw new Exception("No User found. Check spelling of name and password");
+	}
+
+	public static void main(String[] args)
+	{
 		DAOUser user = new DAOUser();
-		//user.changeStatus("test","neuerStatus");
+		// user.changeStatus("test","neuerStatus");
 
 	}
 }
