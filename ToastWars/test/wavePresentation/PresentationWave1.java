d04f59de68bb7357ef72e397f3d70034f58cc8b0
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-13 23:17:46 +0000

    marketresearchReport added

--- ToastWars/test/wavePresentation/PresentationWave1.java
+++ ToastWars/test/wavePresentation/PresentationWave1.java
@@ -31,23 +31,27 @@ public class PresentationWave1 extends TestCase
 
 	@Before
 	public void setUp() throws Exception
-	{
-		toaster1 = new Toaster(10, 10000.00, 12923.00, 1.00, 50000.00, 30000.00, 20000.00, 5000, Type.TYPE1);
-		toaster2 = new Toaster(10, 10000.00, 12923.00, 1.00, 50000.00, 30000.00, 20000.00, 5000, Type.TYPE1);
+	{		
+		// price, marketing, tvInvestment, newsPaperInvestment, radioInvestment,
+		// research, quality, design, efficiency, index,
+		// turnover, cost, profit, marketShare, type
+		toaster1 = new Toaster(10.00, 3.00, 20000.00, 5000.00, 10000.00, 3.00, 5000.00, 5000.00, 5000.00, 9.00, 100000.00, 43000.00, 1000.00, 10000, Type.TYPE1);
+		toaster2 = new Toaster(40.00, 3.00, 30000.00, 10000.00, 20000.00, 3.00, 10000.00, 10000.00, 10000.00, 9.00, 240000.00, 130000.00, 1000.00, 6000, Type.TYPE1);
+
 		toasterList1 = new ArrayList<Toaster>();
 		toasterList2 = new ArrayList<Toaster>();
 		toasterList1.add(toaster1);
 		toasterList2.add(toaster2);
-		company1 = new Company("Unternehmen des ersten Spielers", 50000.00, 30000.00, 20000.00, 100000.00, 5000, toasterList1);
-		company2 = new Company("Unternehmen des zweiten Spielers", 50000.00, 30000.00, 20000.00, 100000.00, 5000, toasterList2);
+		company1 = new Company(1.05, 1.07, 2.10, 10000.00, 50, toasterList1);
+		company2 = new Company(1.05, 1.07, 2.10, 20000.00, 50, toasterList2);
 		master = (Master) UserFactory.createUser("Master", "ADMIN", "ADMIN");
 		group1 = (Group) UserFactory.createUser("Group", "GROUP1", "GROUP1");
 		group2 = (Group) UserFactory.createUser("Group", "GROUP2", "GROUP2");
 		master.startGame(2);
 		group1.setCompany(company1);
 		group2.setCompany(company2);
-		Game.getInstance().addCompany(company1);
-		Game.getInstance().addCompany(company2);
+		Game.getInstance().addGroup(group1);
+		Game.getInstance().addGroup(group2);
 	}
 
 	@After
@@ -72,7 +76,7 @@ public class PresentationWave1 extends TestCase
 		 String TypeOldDescription1 = "Der 1. Typ";
 		 int TypeOldmarketVolume1 = 10000;
 		 double TypeOldrandom1 = 0.05;
-		 double TypeOldfixCostPerMachine1 = 1000.00;
+		 double TypeOldfixCostPerMachine1 = 10000.00;
 		 double TypeOldVariableCost1 = 3.00;
 		 int TypeOldCapacity1 = 4000;
 		 
@@ -82,7 +86,7 @@ public class PresentationWave1 extends TestCase
 		 String TypeOldDescription2 = "Der 1. Typ";;
 		 int TypeOldmarketVolume2 = 10000;
 		 double TypeOldrandom2 = 0.05;
-		 double TypeOldfixCostPerMachine2 = 1000.00;
+		 double TypeOldfixCostPerMachine2 = 10000.00;
 		 double TypeOldVariableCost2 = 3.00;
 		 int TypeOldCapacity2 = 4000;
 		 
@@ -91,13 +95,13 @@ public class PresentationWave1 extends TestCase
 
 			int OldToasterToasterID1 = 1;
 			double OldToasterPrice1  = 10;
-			double OldToasterMarketing1 = 10000.00;
-			double OldToasterResearch1 = 12923.00;
-			double OldToasterIndex1 = 1.00;
-			double OldToasterTurnover1 = 50000.00;
-			double OldToasterCost1 = 30000.00;
-			double OldToasterProfit1 = 20000.00;
-			int OldToasterMarketShare1 = 5000;
+			double OldToasterMarketing1 = 3.00;
+			double OldToasterResearch1 = 3.00;
+			double OldToasterIndex1 = 9.00;
+			double OldToasterTurnover1 = 100000.00;
+			double OldToasterCost1 = 43000.00;
+			double OldToasterProfit1 = 1000.00;
+			int OldToasterMarketShare1 = 10000;
 			Type OldToasterType1 = Type.TYPE1;
 			
 			
@@ -106,14 +110,14 @@ public class PresentationWave1 extends TestCase
 			 
 
 			int OldToasterToasterID2 = 2;
-			double OldToasterPrice2 = 10;
-			double OldToasterMarketing2= 10000.00;
-			double OldToasterResearch2= 12923.00;
-			double OldToasterIndex2= 1.00;
-			double OldToasterTurnover2= 50000.00;
-			double OldToasterCost2 = 30000.00;
-			double OldToasterProfit2= 20000.00;
-			int OldToasterMarketShare2= 5000;
+			double OldToasterPrice2 = 40;
+			double OldToasterMarketing2= 3.00;
+			double OldToasterResearch2= 3.00;
+			double OldToasterIndex2= 9.00;
+			double OldToasterTurnover2= 240000.00;
+			double OldToasterCost2 = 130000.00;
+			double OldToasterProfit2= 1000.00;
+			int OldToasterMarketShare2= 6000;
 			Type OldToasterType2= Type.TYPE1;
 			
 
@@ -122,11 +126,11 @@ public class PresentationWave1 extends TestCase
 
 			int OldCompanyCompanyID1 = 1;
 			String OldCompanyDescription1= "Unternehmen des ersten Spielers";
-			double OldCompanyTurnover1=50000.00;
-			double OldCompanyCost1= 30000.00;
-			double OldCompanyProfit1=20000.00;
-			double OldCompanyCapital1 = 100000.00;
-			int OldCompanyMarketShare1 = 5000;
+			double OldCompanyTurnover1=1.05;
+			double OldCompanyCost1= 1.07;
+			double OldCompanyProfit1=2.1;
+			double OldCompanyCapital1 = 10000.00;
+			int OldCompanyMarketShare1 = 50;
 
 
 			
@@ -134,11 +138,11 @@ public class PresentationWave1 extends TestCase
 
 			int OldCompanyCompanyID2 = 2;
 			String OldCompanyDescription2= "Unternehmen des zweiten Spielers";
-			double OldCompanyTurnover2 = 50000.00;
-			double OldCompanyCost2 = 30000.00;
-			double OldCompanyProfit2 = 20000.00;
-			double OldCompanyCapital2= 100000.00;
-			int OldCompanyMarketShare2 = 5000;
+			double OldCompanyTurnover2 = 1.05;
+			double OldCompanyCost2 = 1.07;
+			double OldCompanyProfit2 = 2.1;
+			double OldCompanyCapital2= 20000.00;
+			int OldCompanyMarketShare2 = 50;
 
 			
 			//Game Old
@@ -233,8 +237,8 @@ public class PresentationWave1 extends TestCase
 		 assertEquals(TypeOldDescription1, Type.TYPE1.getDescription());
 		 assertEquals(TypeOldmarketVolume1, Type.TYPE1.getMarketVolume());
 		 assertEquals(TypeOldrandom1, Type.TYPE1.getRandom());
-		  assertEquals(TypeOldfixCostPerMachine1, Type.TYPE1.getFixCostPerMachine());
-		 assertEquals(TypeOldVariableCost1, Type.TYPE1.getVariableCost());
+		  assertEquals(TypeOldfixCostPerMachine1, Type.TYPE1.getFixCosts());
+		 assertEquals(TypeOldVariableCost1, Type.TYPE1.getVariableCosts());
 		 assertEquals(TypeOldCapacity1, Type.TYPE1.getCapacity());
 		 
 		 
@@ -242,8 +246,8 @@ public class PresentationWave1 extends TestCase
 		 assertEquals(TypeOldDescription2, Type.TYPE1.getDescription());
 		 assertEquals(TypeOldmarketVolume2, Type.TYPE1.getMarketVolume());
 		 assertEquals(TypeOldrandom2, Type.TYPE1.getRandom());
-		  assertEquals(TypeOldfixCostPerMachine2, Type.TYPE1.getFixCostPerMachine());
-		 assertEquals(TypeOldVariableCost2, Type.TYPE1.getVariableCost());
+		  assertEquals(TypeOldfixCostPerMachine2, Type.TYPE1.getFixCosts());
+		 assertEquals(TypeOldVariableCost2, Type.TYPE1.getVariableCosts());
 		 assertEquals(TypeOldCapacity2, Type.TYPE1.getCapacity());
 		 
 		 // Toaster 1  Old
@@ -279,7 +283,6 @@ public class PresentationWave1 extends TestCase
 			//Company 1 Old
 		 
 		 assertEquals(OldCompanyCompanyID1, company1.getCompanyID());
-		 assertEquals(OldCompanyDescription1, company1.getDescription());
 		 assertEquals(OldCompanyTurnover1, company1.getTurnover());
 		 assertEquals(OldCompanyCost1, company1.getCost());
 		 assertEquals(OldCompanyProfit1, company1.getProfit());
@@ -290,7 +293,6 @@ public class PresentationWave1 extends TestCase
 			//Company 2 Old
 		 
 		 assertEquals(OldCompanyCompanyID2, company2.getCompanyID());
-		 assertEquals(OldCompanyDescription2, company2.getDescription());
 		 assertEquals(OldCompanyTurnover2, company2.getTurnover());
 		 assertEquals(OldCompanyCost2, company2.getCost());
 		 assertEquals(OldCompanyProfit2, company2.getProfit());
@@ -326,8 +328,8 @@ public class PresentationWave1 extends TestCase
 		 assertEquals(TypeNewDescription1, Type.TYPE1.getDescription());
 		 assertEquals(TypeNewmarketVolume1, Type.TYPE1.getMarketVolume());
 		 assertEquals(TypeNewrandom1, Type.TYPE1.getRandom());
-		  assertEquals(TypeNewfixCostPerMachine1, Type.TYPE1.getFixCostPerMachine());
-		 assertEquals(TypeNewVariableCost1, Type.TYPE1.getVariableCost());
+		  assertEquals(TypeNewfixCostPerMachine1, Type.TYPE1.getFixCosts());
+		 assertEquals(TypeNewVariableCost1, Type.TYPE1.getVariableCosts());
 		 assertEquals(TypeNewCapacity1, Type.TYPE1.getCapacity());
 		 
 		 
@@ -335,8 +337,8 @@ public class PresentationWave1 extends TestCase
 		 assertEquals(TypeNewDescription2, Type.TYPE1.getDescription());
 		 assertEquals(TypeNewmarketVolume2, Type.TYPE1.getMarketVolume());
 		 assertEquals(TypeNewrandom2, Type.TYPE1.getRandom());
-		  assertEquals(TypeNewfixCostPerMachine2, Type.TYPE1.getFixCostPerMachine());
-		 assertEquals(TypeNewVariableCost2, Type.TYPE1.getVariableCost());
+		  assertEquals(TypeNewfixCostPerMachine2, Type.TYPE1.getFixCosts());
+		 assertEquals(TypeNewVariableCost2, Type.TYPE1.getVariableCosts());
 		 assertEquals(TypeNewCapacity2, Type.TYPE1.getCapacity());
 		 
 		 // Toaster 1  New
@@ -372,7 +374,6 @@ public class PresentationWave1 extends TestCase
 			//Company 1 New
 		 
 		 assertEquals(NewCompanyCompanyID1, company1.getCompanyID());
-		 assertEquals(NewCompanyDescription1, company1.getDescription());
 		 assertEquals(NewCompanyTurnover1, company1.getTurnover());
 		 assertEquals(NewCompanyCost1, company1.getCost());
 		 assertEquals(NewCompanyProfit1, company1.getProfit());
@@ -383,7 +384,6 @@ public class PresentationWave1 extends TestCase
 			//Company 2 New
 		 
 		 assertEquals(NewCompanyCompanyID2, company2.getCompanyID());
-		 assertEquals(NewCompanyDescription2, company2.getDescription());
 		 assertEquals(NewCompanyTurnover2, company2.getTurnover());
 		 assertEquals(NewCompanyCost2, company2.getCost());
 		 assertEquals(NewCompanyProfit2, company2.getProfit());
@@ -421,15 +421,15 @@ public class PresentationWave1 extends TestCase
 		double profit;
 		double capital;
 		double marketsharePercent;
-		for (int i = 0; i < Group.getGroupList().size(); i++)
+		for (int i = 0; i < game.getGroupList().size(); i++)
 		{
-			companyID = Group.getGroupList().get(i).getCompany().getCompanyID();
-			marketshare = Group.getGroupList().get(i).getCompany().getMarketShare();
+			companyID = game.getGroupList().get(i).getCompany().getCompanyID();
+			marketshare = game.getGroupList().get(i).getCompany().getMarketShare();
 			marketsharePercent = marketshare / (double)Type.TYPE1.getMarketVolume() * 100;
-			turnover = Group.getGroupList().get(i).getCompany().getTurnover();
-			cost = Group.getGroupList().get(i).getCompany().getCost();
-			profit = Group.getGroupList().get(i).getCompany().getProfit();
-			capital = Group.getGroupList().get(i).getCompany().getCapital();
+			turnover = game.getGroupList().get(i).getCompany().getTurnover();
+			cost = game.getGroupList().get(i).getCompany().getCost();
+			profit = game.getGroupList().get(i).getCompany().getProfit();
+			capital = game.getGroupList().get(i).getCompany().getCapital();
 			System.out.printf("%1$,15d | %2$,18d | %3$,13.2f %% | %4$,13.2f € | %5$,13.2f € | %6$,13.2f € | %7$,13.2f € | \n", companyID, marketshare, marketsharePercent, turnover, cost, profit, capital);
 		}
 		System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
