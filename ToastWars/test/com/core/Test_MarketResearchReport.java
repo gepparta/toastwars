86d2472a6e2a99f64bf69b54182ff95b14b835ac
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-14 15:11:20 +0000

    fertisch

--- ToastWars/test/com/core/Test_MarketResearchReport.java
+++ ToastWars/test/com/core/Test_MarketResearchReport.java
@@ -18,10 +18,15 @@ public class Test_MarketResearchReport extends TestCase
 	private Toaster toaster1;
 	private Toaster toaster2;
 	private Toaster toaster3;
+	private Toaster toaster4;
+	private Toaster toaster5;
+	private Toaster toaster6;
 	private ArrayList<Toaster> toasterList1;
 	private ArrayList<Toaster> toasterList2;
+	private ArrayList<Toaster> toasterList3;
 	private Company company1;
 	private Company company2;
+	private Company company3;
 	private Master master;
 
 	@Before
@@ -36,21 +41,34 @@ public class Test_MarketResearchReport extends TestCase
 		toaster2 = new Toaster(40.00, 3.00, 30000.00, 10000.00, 20000.00, 3.00, 10000.00, 10000.00, 10000.00, 9.00, 240000.00, 130000.00, 1000.00, 6000, Type.TYPE2);
 		toaster3 = new Toaster(150.00, 3.00, 40000.00, 20000.00, 30000.00, 3.00, 15000.00, 15000.00, 15000.00, 9.00, 375000.00, 155000.00, 1000.00, 2500, Type.TYPE3);
 
+		toaster4 = new Toaster(12.00, 3.00, 20000.00, 5000.00, 10000.00, 3.00, 5000.00, 5000.00, 5000.00, 9.00, 100000.00, 43000.00, 1000.00, 10000, Type.TYPE1);
+		toaster5 = new Toaster(42.00, 3.00, 30000.00, 10000.00, 20000.00, 3.00, 10000.00, 10000.00, 10000.00, 9.00, 240000.00, 130000.00, 1000.00, 6000, Type.TYPE2);
+		toaster6 = new Toaster(152.00, 3.00, 40000.00, 20000.00, 30000.00, 3.00, 15000.00, 15000.00, 15000.00, 9.00, 375000.00, 155000.00, 1000.00, 2500, Type.TYPE3);
+		
 		toasterList1 = new ArrayList<Toaster>();
 		toasterList1.add(toaster1);
 		toasterList2 = new ArrayList<Toaster>();
 		toasterList2.add(toaster2);
 		toasterList2.add(toaster3);
+		
+		toasterList3 = new ArrayList<Toaster>();
+		toasterList3.add(toaster4);
+		toasterList3.add(toaster5);
+		toasterList3.add(toaster6);
+		
 		// turnover, cost, profit, capital, marketShare, toasterList
 		company1 = new Company(1.05, 1.07, 2.10, 100000.00, 50, toasterList1);
 		company2 = new Company(1.05, 1.07, 2.10, 100000.00, 50, toasterList2);
+		company3 = new Company(1.05, 1.07, 2.10, 100000.00, 50, toasterList3);
 
 		master = (Master) UserFactory.createUser("Master", "ADMIN", "ADMIN");
-		master.startGame(2);
+		master.startGame(3);
 		Game.getInstance().getGroupList().get(0).setCompany(company1);
 		Game.getInstance().getGroupList().get(1).setCompany(company2);
+		Game.getInstance().getGroupList().get(2).setCompany(company3);
 		Game.getInstance().getGroupList().get(0).getCompany().setToasterList(toasterList1);
 		Game.getInstance().getGroupList().get(1).getCompany().setToasterList(toasterList2);
+		Game.getInstance().getGroupList().get(2).getCompany().setToasterList(toasterList3);
 	}
 
 	@After
@@ -76,5 +94,7 @@ public class Test_MarketResearchReport extends TestCase
 		assertNotNull(Game.getInstance().getGroupList().get(0).getCompany().getMarketResearchReport());
 		System.out.println(Game.getInstance().getGroupList().get(0).getCompany().getMarketResearchReport().toString());
 		System.out.println(Game.getInstance().getGroupList().get(0).getCompany().toString());
+		System.out.println(Game.getInstance().getGroupList().get(0).getCompany().getToasterList().get(0).toString());
+
 	}
 }
