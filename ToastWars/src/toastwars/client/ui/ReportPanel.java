0568e4e1bc9dfd6fb537ad011fa0af7cc7409755
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-18 15:16:50 +0000

    fehlerhaft

--- ToastWars/src/toastwars/client/ui/ReportPanel.java
+++ ToastWars/src/toastwars/client/ui/ReportPanel.java
@@ -1,5 +1,14 @@
 package toastwars.client.ui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import toastwars.client.Controller;
+import toastwars.server.datamodel.user.Group;
+
 import com.gwtext.client.widgets.Panel;
 import com.gwtext.client.widgets.layout.HorizontalLayout;
 import com.rednels.ofcgwt.client.ChartWidget;
@@ -10,11 +19,27 @@ import com.rednels.ofcgwt.client.model.elements.BarChart;
 import com.rednels.ofcgwt.client.model.elements.PieChart;
 import com.rednels.ofcgwt.client.model.elements.BarChart.BarStyle;
 
-public class ReportPanel extends Panel {
+public class ReportPanel extends Panel
+{
 
-	private static ReportPanel	reportPanel;
+	private static ReportPanel reportPanel;
+	private final String COLOR_1 = "#ff0000";
+	private final String COLOR_2 = "#00ff00";
+	private final String COLOR_3 = "#0000ff";
+	private final String COLOR_4 = "#ff9900";
+	private final String COLOR_5 = "#ff0099";
+	private final String COLOR_6 = "#99ff00";
+	private final String COLOR_7 = "#9900ff";
+	private final String COLOR_8 = "#009900";
+	private final String COLOR_9 = "#99ffff";
+	private final String COLOR_10 = "#ffff99";
 
-	private ReportPanel() {
+	private Group group;
+
+	private ReportPanel()
+	{
+		group = (Group) Controller.getInstance().getUser();
+
 		setTitle("Analyse-Bericht");
 		setPaddings(15);
 
@@ -27,18 +52,21 @@ public class ReportPanel extends Panel {
 		add(horizPanel);
 	}
 
-	public static ReportPanel getInstance() {
+	public static ReportPanel getInstance()
+	{
 		if (reportPanel == null)
 			reportPanel = new ReportPanel();
 		return reportPanel;
 	}
 
-	private ChartWidget createPieChart() {
+	private ChartWidget createPieChart()
+	{
 		ChartWidget chart = new ChartWidget();
 
-		ChartData cd = new ChartData("Marktanteile",
-				"font-size: 14px; text-align: center; border: 1px solid gold;");
-		cd.setBackgroundColour("#000000");
+		ChartData cd = new ChartData(
+				"Marktanteile",
+				"filter:Alpha(opacity=100, finishopacity=80, startx=10, finishx=484, style=1); -moz-opacity: 0.9 ; font-size: 14px; font-family: Verdana; text-align: center;");
+		cd.setBackgroundColour("#ffffff");
 
 		PieChart pie = new PieChart();
 		pie.setAlpha(1f);
@@ -46,11 +74,13 @@ public class ReportPanel extends Panel {
 		pie.setTooltip("#label#<br>#val# Toaster<br>#percent#");
 		pie.setAnimate(false);
 		pie.setGradientFill(true);
-		pie.setColours("#ff0000", "#00ff00", "#0000ff", "#ff9900");
-		pie.addSlices(new PieChart.Slice(1000, "Gruppe 1"));
-		pie.addSlices(new PieChart.Slice(2000, "Gruppe 2"));
-		pie.addSlices(new PieChart.Slice(6000, "Gruppe 3"));
-		pie.addSlices(new PieChart.Slice(1000, "Gruppe 4"));
+		pie.setColours(COLOR_1, COLOR_2);
+		pie.addSlices(new PieChart.Slice(group.getCompany().getMarketShare(), Controller.getInstance().getUser().getUsername()));
+		int restMarketShare = 0;
+		for (int i = 0; i < group.getCompany().getToasterList().size(); i++)
+			restMarketShare += group.getCompany().getToasterList().get(i).getType().getMarketVolume();
+		restMarketShare -= group.getCompany().getMarketShare();
+		pie.addSlices(new PieChart.Slice(restMarketShare, "Rest"));
 
 		cd.addElements(pie);
 
@@ -59,14 +89,27 @@ public class ReportPanel extends Panel {
 		return chart;
 	}
 
-	private ChartWidget createBarChart() {
+	private ChartWidget createBarChart()
+	{
 		ChartWidget chart = new ChartWidget();
-		ChartData cd = new ChartData("Kapital nach Runde 1",
-				"font-size: 14px; font-family: Verdana; text-align: center;");
+		ChartData cd = new ChartData("Kapital nach Runde 1", "font-size: 14px; font-family: Verdana; text-align: center;");
 		cd.setBackgroundColour("#ffffff");
 
 		XAxis xa = new XAxis();
-		xa.setLabels("Gruppe 1", "Gruppe 2", "Gruppe 3", "Gruppe 4");
+		List<String> labels = new ArrayList<String>();
+		List<Number> bchartValues = new ArrayList<Number>();
+		Map<Number, String> capitalList = group.getCompany().getKapitalOverview(Controller.getInstance().getGame());
+		Set<Number> keySet = capitalList.keySet();
+		Iterator<Number> iter = keySet.iterator();
+		while (iter.hasNext())
+		{
+			Number key = iter.next();
+			String value = capitalList.get(key);
+			labels.add(value);
+			bchartValues.add(key);
+		}
+
+		xa.setLabels(labels);
 		xa.setMax(3);
 		cd.setXAxis(xa);
 
@@ -78,7 +121,7 @@ public class ReportPanel extends Panel {
 		BarChart bchart = new BarChart(BarStyle.GLASS);
 		bchart.setColour("#00aa00");
 		bchart.setTooltip("#val# &#8364;");
-		bchart.addValues(40000, 60000, 30000, 150000);
+		bchart.addValues(bchartValues);
 		cd.addElements(bchart);
 
 		chart.setSize("300", "250");
@@ -86,5 +129,4 @@ public class ReportPanel extends Panel {
 
 		return chart;
 	}
-
 }
