ddd2ce0c52386c18698ac6755506277f4a3b627d
waldemar.geppart <waldemar.geppart@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-20 22:35:16 +0000

    Add method for creation of new toasters

--- ToastWars/src/toastwars/client/ToastWarsServiceAsync.java
+++ ToastWars/src/toastwars/client/ToastWarsServiceAsync.java
@@ -1,6 +1,9 @@
 package toastwars.client;
 
+import java.util.ArrayList;
+
 import toastwars.server.datamodel.core.Game;
+import toastwars.server.datamodel.core.Toaster;
 import toastwars.server.datamodel.user.Group;
 import toastwars.server.datamodel.user.IUser;
 
@@ -10,10 +13,6 @@ public interface ToastWarsServiceAsync {
 
 	public void login(String name, String pwd, AsyncCallback callback);
 
-	public void getCurrentGame(AsyncCallback callback);
-
-	public void save(Group group, AsyncCallback callback);
-
 	public void logout(String name, String pwd, AsyncCallback callback);
 
 	public void startGame(int userAmount, AsyncCallback callback);
@@ -21,4 +20,11 @@ public interface ToastWarsServiceAsync {
 	public void endGame(AsyncCallback callback);
 
 	public void simulate(AsyncCallback callback);
+
+	public void getCurrentGame(AsyncCallback callback);
+
+	public void save(Group group, AsyncCallback callback);
+
+	public void createNewToaster(ArrayList<Toaster> toasterList,
+			int companyID, AsyncCallback callback);
 }
