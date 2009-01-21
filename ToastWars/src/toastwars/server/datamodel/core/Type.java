cf329bd77e04faef6019251c05a91be1e7137fca
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-21 13:13:16 +0000

    setter wech

--- ToastWars/src/toastwars/server/datamodel/core/Type.java
+++ ToastWars/src/toastwars/server/datamodel/core/Type.java
@@ -3,24 +3,19 @@ package toastwars.server.datamodel.core;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public enum Type implements IsSerializable {
-	
-	
-	// description, marketVolume, random, fixCost, stepCosts, variableCost,capacity,
-	// tvInvestmentPlus, radioInvestmentPlus, newspaperInvestmentPlus
-	// qualityInvestmentPlus, designInvestmentPlus, ecologyInvestmentPlus
+	// description, marketVolume, random, fixCost, stepCosts, variableCost,
+	// capacity,
+	// tvInvestmentPlus, radioInvestmentPlus newspaperInvestmentPlus
+	// qualityInvestmentPlus
+	// designInvestmentPlus ecologyInvestmentPlus
 
-	
-	//Definition  der drei verschiedenen Toastertypen und deren Eigenschaften.
-	
-	TYPE1("Millenniums-Toaster", 10000, 0.05, 10000.00, 1000.00, 2.00, 4000,
+	TYPE1("Millenniums-Toaster", 10000, 0.05, 10000.00, 1000.00, 3.00, 2000,
 			20000, 10000, 5000, 5000, 5000, 5000, 5, 20),
-	TYPE2("TIE-Toaster", 6000, 0.08, 20000.00, 5000.00, 10.00, 2000, 30000,
+	TYPE2("TIE-Toaster", 6000, 0.08, 20000.00, 5000.00, 15.00, 1000, 30000,
 			20000, 10000, 10000, 10000, 10000, 30, 60),
 	TYPE3("Star-Toaster", 2500, 0.10, 25000.00, 8000.00, 40.00, 500, 40000,
 			30000, 20000, 15000, 15000, 15000, 130, 200);
 
-	//Definition der Attribute
-	
 	private String	description;
 	private int		marketVolume;
 	private double	random;
@@ -37,9 +32,6 @@ public enum Type implements IsSerializable {
 	private int		minPrice;
 	private int		maxPrice;
 
-	
-	//Konstruktoren
-	
 	private Type(String description, int marketVolume, double random,
 			double fixCosts, double stepCosts, double variableCosts,
 			int capacity, double tvInvestmentPlus, double radioInvestmentPlus,
@@ -63,9 +55,6 @@ public enum Type implements IsSerializable {
 		this.maxPrice = maxPrice;
 	}
 
-	
-	//GET Methoden
-	
 	public int getCapacity() {
 		return capacity;
 	}
@@ -126,70 +115,6 @@ public enum Type implements IsSerializable {
 		return variableCosts;
 	}
 
-	//Set-Methoden
-	
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public void setDesignInvestmentPlus(double designInvestmentPlus) {
-		this.designInvestmentPlus = designInvestmentPlus;
-	}
-
-	public void setEcologyInvestmentPlus(double ecologyInvestmentPlus) {
-		this.ecologyInvestmentPlus = ecologyInvestmentPlus;
-	}
-
-	public void setFixCosts(double fixCosts) {
-		this.fixCosts = fixCosts;
-	}
-
-	public void setMarketVolume(int marketVolume) {
-		this.marketVolume = marketVolume;
-	}
-
-	public void setMaxPrice(int maxPrice) {
-		this.maxPrice = maxPrice;
-	}
-
-	public void setMinPrice(int minPrice) {
-		this.minPrice = minPrice;
-	}
-
-	public void setNewspaperInvestmentPlus(double newspaperInvestmentPlus) {
-		this.newspaperInvestmentPlus = newspaperInvestmentPlus;
-	}
-
-	public void setQualityInvestmentPlus(double qualityInvestmentPlus) {
-		this.qualityInvestmentPlus = qualityInvestmentPlus;
-	}
-
-	public void setRadioInvestmentPlus(double radioInvestmentPlus) {
-		this.radioInvestmentPlus = radioInvestmentPlus;
-	}
-
-	public void setRandom(double random) {
-		this.random = random;
-	}
-
-	public void setStepCosts(double stepCosts) {
-		this.stepCosts = stepCosts;
-	}
-
-	public void setTvInvestmentPlus(double tvInvestmentPlus) {
-		this.tvInvestmentPlus = tvInvestmentPlus;
-	}
-
-	public void setVariableCosts(double variableCosts) {
-		this.variableCosts = variableCosts;
-	}
-
-	
-	//Methoden nur für die JUNIT Tests
 	
 	public String toString() {
 
@@ -204,4 +129,4 @@ public enum Type implements IsSerializable {
 		return s;
 	}
 
-}// class Type
+}
