8c2008e219aefbb148e2481d11ccfcb641ccf87f
tobiasmerx@gmx.de <tobiasmerx@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-22 13:51:18 +0000

    Klasse DAOScenario hinzugefügt

---
+++ trunk/ToastWars/src/toastwars/server/dao/DAOScenario.java
@@ -0,0 +1,39 @@
+package toastwars.server.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet; //import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import toastwars.server.datamodel.core.Game;
+import toastwars.server.datamodel.core.Stock;
+
+public class DAOScenario
+{
+
+	public static String getScenarioByRound(int round, Connection con)
+	{
+
+		try
+		{
+			// Abfrage definieren
+			String query = "SELECT Scenario.Description FROM Scenario WHERE  Round = " + round + ";";
+			Statement stmt = con.createStatement();
+			ResultSet rst = stmt.executeQuery(query);
+
+			// Zeileninhalt ermitteln
+			while (rst.next())
+			{
+				String description = rst.getString(1);
+				rst.close();
+				stmt.close();
+				System.out.println(description);
+				return description;
+			}
+			return null;
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
