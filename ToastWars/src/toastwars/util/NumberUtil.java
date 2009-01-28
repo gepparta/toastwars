dfc420567b0707e49f4298173d64281c0df77fc4
waldemar.geppart <waldemar.geppart@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-28 13:20:32 +0000

    Zahlen in der Tabelle des Spielleiters jetzt formatiert

--- ToastWars/src/toastwars/util/NumberUtil.java
+++ ToastWars/src/toastwars/util/NumberUtil.java
@@ -23,4 +23,35 @@ public class NumberUtil {
 		int i = (int) Math.floor(d / stellen);
 		return i * stellen;
 	}
+
+	public static String formatToEuro(Number n) {
+		StringBuffer s = new StringBuffer(n.toString());
+
+		int i = s.lastIndexOf(".");
+
+		// replace dot with comma or append zeros
+		if (i != -1)
+			s.replace(i, i + 1, ",");
+		else
+			s.append(",00");
+
+		// insert thousand separator
+		i = s.lastIndexOf(",");
+		StringBuffer left = new StringBuffer(s.substring(0, i));
+
+		for (int j = left.length() - 3; j > 0; j -= 3) {
+			s.insert(j, '.');
+		}
+
+		// cut after 2
+		i = s.lastIndexOf(",");
+		if ((s.length() - i) > 3)
+			s = s.delete(i + 3, s.length());
+		if ((s.length() - i) < 3)
+			s = s.append('0');
+
+		s.append(" &euro;");
+
+		return s.toString();
+	}
 }
