b4af2fa8b7fce8eca7c37383195750aaf5c84cb8
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-01-30 13:00:27 +0000

    kosten für neue t-typen  werden nun berücksichtigt

--- ToastWars/src/toastwars/server/datamodel/core/Company.java
+++ ToastWars/src/toastwars/server/datamodel/core/Company.java
@@ -231,21 +231,8 @@ public class Company implements IsSerializable
 			totalInvestmentCosts += 5000.0;
 		}
 		
-//		wenn ein Toaster Typ 2 erzeugt wurde, findet hier die Umbuchung der Kosten statt
-		for (Toaster toaster : toasterList)
-		{
-//			TODO: DAO von Merx aufrufen
-			if(true == false)
-			{
-				if(toaster.getType()==Type.TYPE2)
-					totalInvestmentCosts+=15000;
-				if(toaster.getType()==Type.TYPE3)
-					totalInvestmentCosts+=25000;
-			}
-		}
-		
 		this.setCapital(this.getCapital()+totalInvestmentCosts);
-		this.setCost(tmpCost+totalInvestmentCosts+ stock.getTotalStockCosts());
+		this.setCost(getCost()+tmpCost+totalInvestmentCosts+ stock.getTotalStockCosts());
 	}
 
 	// In dieser Methode wird die Indexberechnung aller Toaster dieser Company ausgeführt.
