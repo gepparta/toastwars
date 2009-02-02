39e2cc5502c343086fc58a014c56c833f08443c3
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-02-02 21:49:37 +0000

    Group Objekt nur für die Standarddarstellung implementiert

---
+++ ToastWars/src/toastwars/client/comet/CometController.java
@@ -0,0 +1,49 @@
+package toastwars.client.comet;
+
+import rocket.remoting.client.CometCallback;
+import rocket.remoting.client.GwtSerializationCometClient;
+import toastwars.client.ui.master.MasterPanel;
+import toastwars.server.datamodel.core.Game;
+import com.google.gwt.core.client.GWT;
+
+public class CometController
+{
+	static GameCometClient cometClient = (GameCometClient) GWT.create(GameCometClient.class);
+	public static void startPushService()
+	{
+		cometClient.setServiceEntryPoint(GWT.getModuleBaseURL() + "CometService");
+		cometClient.setCallback(new CometCallback()
+		{
+			public void onPayload(Object object)
+			{
+				Game game = (Game) object;
+				MasterPanel.getInstance().refreshGrid(game);
+			}
+
+			public void onTerminate()
+			{
+				System.out.println("Comet terminated");
+			}
+
+			public void onFailure(Throwable throwable)
+			{
+				throwable.printStackTrace();
+			}
+		});
+
+		cometClient.start();
+	}
+	
+	public static void endPushService()
+	{
+		cometClient.stop();
+	}
+
+	/**
+	 * @comet-payloadType toastwars.server.datamodel.core.Game
+	 */
+	abstract public static class GameCometClient extends GwtSerializationCometClient
+	{
+
+	}
+}
