fb1e1a6521307ce31c79a59db13be2f1b249ea08
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-02-02 21:49:23 +0000

    Group Objekt nur für die Standarddarstellung implementiert

--- ToastWars/src/toastwars/client/ToastWarsServiceAsync.java
+++ ToastWars/src/toastwars/client/ToastWarsServiceAsync.java
@@ -1,12 +1,8 @@
 package toastwars.client;
 
 import java.util.ArrayList;
-
-import toastwars.server.datamodel.core.Game;
 import toastwars.server.datamodel.core.Toaster;
 import toastwars.server.datamodel.user.Group;
-import toastwars.server.datamodel.user.IUser;
-
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface ToastWarsServiceAsync {
@@ -21,7 +17,7 @@ public interface ToastWarsServiceAsync {
 
 	public void simulate(AsyncCallback callback);
 
-	public void getCurrentGame(AsyncCallback callback);
+	public void getPreviousGame(AsyncCallback callback);
 
 	public void save(Group group, AsyncCallback callback);
 
