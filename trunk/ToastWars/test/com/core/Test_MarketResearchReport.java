75e9ea6300c5551913f46d3f687744c1cfe8011e
alexander.geppart@freenet.de <alexander.geppart@freenet.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-02-06 10:00:49 +0000

    Test für Marketresearchreport erstellen

--- trunk/ToastWars/test/com/core/Test_MarketResearchReport.java
+++ trunk/ToastWars/test/com/core/Test_MarketResearchReport.java
@@ -1,6 +1,9 @@
 package com.core;
 
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -8,10 +11,13 @@ import org.junit.Test;
 import toastwars.server.ToastWarsServiceImpl;
 import toastwars.server.dao.DAOGame;
 import toastwars.server.dao.DAOUser;
+import toastwars.server.dao.DBConnection;
 import toastwars.server.datamodel.core.Company;
 import toastwars.server.datamodel.core.Game;
+import toastwars.server.datamodel.core.Stock;
 import toastwars.server.datamodel.core.Toaster;
 import toastwars.server.datamodel.core.Type;
+import toastwars.server.datamodel.user.Group;
 import toastwars.server.datamodel.user.Master;
 import toastwars.server.datamodel.user.UserFactory;
 
@@ -29,6 +35,15 @@ public class Test_MarketResearchReport extends TestCase
 	private Company company1;
 	private Company company2;
 	private Company company3;
+	
+	private Group group1;
+	private Group group2;
+	private Group group3;
+	
+	private Stock stock1;
+	private Stock stock2;
+	private Stock stock3;
+	
 	private Master master;
 	
 	private ToastWarsServiceImpl impl;
@@ -37,19 +52,29 @@ public class Test_MarketResearchReport extends TestCase
 	public void setUp() throws Exception
 	{
 		impl = new ToastWarsServiceImpl();
-		DAOGame.resetGame();
-		// price, 
-		// marketing, tvInvestment, newsPaperInvestment, radioInvestment,
-		// research, quality, design, efficiency, 
-		// index, turnover, cost, profit, marketShare, type
-		toaster1 = new Toaster(10.00, 3.00, 20000.00, 5000.00, 10000.00, 3.00, 5000.00, 5000.00, 5000.00, 9.00, 100000.00, 43000.00, 1000.00, 10000, Type.TYPE1);
-		toaster2 = new Toaster(40.00, 3.00, 30000.00, 10000.00, 20000.00, 3.00, 10000.00, 10000.00, 10000.00, 9.00, 240000.00, 130000.00, 1000.00, 6000, Type.TYPE2);
-		toaster3 = new Toaster(150.00, 3.00, 40000.00, 20000.00, 30000.00, 3.00, 15000.00, 15000.00, 15000.00, 9.00, 375000.00, 155000.00, 1000.00, 2500, Type.TYPE3);
+//		Connection con = DBConnection.getInstance().connectToDB();
+//		DAOGame.resetGame(con);
+//		// close DB connection
+//		DBConnection.getInstance().closeConnectionToDB(con);
 
-		toaster4 = new Toaster(12.00, 3.00, 20000.00, 5000.00, 10000.00, 3.00, 5000.00, 5000.00, 5000.00, 9.00, 100000.00, 43000.00, 1000.00, 10000, Type.TYPE1);
-		toaster5 = new Toaster(42.00, 3.00, 30000.00, 10000.00, 20000.00, 3.00, 10000.00, 10000.00, 10000.00, 9.00, 240000.00, 130000.00, 1000.00, 6000, Type.TYPE2);
-		toaster6 = new Toaster(152.00, 3.00, 40000.00, 20000.00, 30000.00, 3.00, 15000.00, 15000.00, 15000.00, 9.00, 375000.00, 155000.00, 1000.00, 2500, Type.TYPE3);
+// 	     price,  index,  turnover,  cost,
+//		 profit,  marketShare,  type,  marketing,
+//		 tvInvestment,  newspaperInvestment,
+//		 radioInvestment,  tvInvestmentKum,
+//		 newspaperInvestmentKum,  radioInvestmentKum,
+//		 research,  qualityInvestment,  designInvestment,
+//		 ecologyInvestment,  qualityInvestmentKum,
+//		 designInvestmentKum,  ecologyInvestmentKum, production
 		
+		toaster1 = new Toaster(10.0, 9.00, 100000.00,50000.00,1000.00,10000,Type.TYPE1,3.00,1.00,1.00,1.00,0.00,0.00,0.00,3.00,1.00,1.00,1.00,0.00,0.00,0.00, 1000);
+		toaster2 = new Toaster(40.00, 9.00, 100000.00,50000.00,1000.00,6000,Type.TYPE2,3.00,1.00,1.00,1.00,0.00,0.00,0.00,3.00,1.00,1.00,1.00,0.00,0.00,0.00, 1000);
+		toaster3 = new Toaster(150.00, 9.00, 100000.00,50000.00,1000.00,2500,Type.TYPE3,3.00,1.00,1.00,1.00,0.00,0.00,0.00,3.00,1.00,1.00,1.00,0.00,0.00,0.00, 1000);
+
+		toaster4 = new Toaster(12.0, 9.00, 100000.00,50000.00,1000.00,10000,Type.TYPE1,3.00,1.00,1.00,1.00,0.00,0.00,0.00,3.00,1.00,1.00,1.00,0.00,0.00,0.00, 1000);
+		toaster5 = new Toaster(42.00, 9.00, 100000.00,50000.00,1000.00,6000,Type.TYPE2,3.00,1.00,1.00,1.00,0.00,0.00,0.00,3.00,1.00,1.00,1.00,0.00,0.00,0.00, 1000);
+		toaster6 = new Toaster(152.00, 9.00, 100000.00,50000.00,1000.00,2500,Type.TYPE3,3.00,1.00,1.00,1.00,0.00,0.00,0.00,3.00,1.00,1.00,1.00,0.00,0.00,0.00, 1000);
+
+				
 		toasterList1 = new ArrayList<Toaster>();
 		toasterList1.add(toaster1);
 		toasterList2 = new ArrayList<Toaster>();
@@ -61,13 +86,28 @@ public class Test_MarketResearchReport extends TestCase
 		toasterList3.add(toaster5);
 		toasterList3.add(toaster6);
 		
-		// turnover, cost, profit, capital, marketShare, toasterList
-		company1 = new Company(1.05, 1.07, 2.10, 100000.00, 50, toasterList1);
-		company2 = new Company(1.05, 1.07, 2.10, 100000.00, 50, toasterList2);
-		company3 = new Company(1.05, 1.07, 2.10, 100000.00, 50, toasterList3);
+		stock1 = new Stock(0,0,0,0.00);
+		stock2 = new Stock(0,0,0,0.00);
+		stock3 = new Stock(0,0,0,0.00);
+		
+		
+//		 turnover,  cost,  profit,  capital,  marketShare,  stock,  toasterList
 
+		company1 = new Company(100000.00,20000.00, 60000.00,100000.00, 10000,stock1,toasterList1);
+		company2 = new Company(100000.00, 30000.00, 60000.00,100000.00,10000,stock2,toasterList2);
+		company3 = new Company(100000.00, 30000.00, 60000.00,100000.00,10000,stock3,toasterList3);
+		
 		master = (Master) UserFactory.createUser("Master", "ADMIN", "ADMIN");
-		impl.startGame(3);
+		group1 = (Group)UserFactory.createUser("Group", "group1","group1" );
+		group2 = (Group)UserFactory.createUser("Group", "group2","group2" );
+		group3 = (Group)UserFactory.createUser("Group", "group3","group3" );
+
+
+		Game.getInstance(3);
+		Game.getInstance().addGroup(group1);
+		Game.getInstance().addGroup(group2);
+		Game.getInstance().addGroup(group3);
+		
 		Game.getInstance().getGroupList().get(0).setCompany(company1);
 		Game.getInstance().getGroupList().get(1).setCompany(company2);
 		Game.getInstance().getGroupList().get(2).setCompany(company3);
@@ -86,23 +126,19 @@ public class Test_MarketResearchReport extends TestCase
 		toasterList2 = null;
 		company1 = null;
 		company2 = null;
-		System.gc();
+		
+		master = null;
+		Game.destroyGame();
 	}
 
 	@Test
 	public void testGenerateMarketResearchReport()
 	{
 		Game.getInstance().getGroupList().get(0).getCompany().setMarketResearchReportON(true);
-		assertNull(Game.getInstance().getGroupList().get(0).getCompany().getMarketResearchReport());
+		assertEquals(new ArrayList<ArrayList<List<String>>>(), Game.getInstance().getGroupList().get(0).getCompany().getReportListe());
 		impl.simulate();
-		assertNotNull(Game.getInstance());
-		assertNotNull(Game.getInstance().getGroupList());
-		assertNotNull(Game.getInstance().getGroupList().get(0).getCompany());
-		assertFalse(Game.getInstance().getGroupList().get(0).getCompany().isMarketResearchReportON());
-		assertNotNull(Game.getInstance().getGroupList().get(0).getCompany().getMarketResearchReport());
-		System.out.println(Game.getInstance().getGroupList().get(0).getCompany().getMarketResearchReport().toString());
-		System.out.println(Game.getInstance().getGroupList().get(0).getCompany().toString());
-		System.out.println(Game.getInstance().getGroupList().get(0).getCompany().getToasterList().get(0).toString());
 
+		assertFalse(Game.getInstance().getGroupList().get(0).getCompany().isMarketResearchReportON());
+		assertNotNull(Game.getInstance().getGroupList().get(0).getCompany().getReportListe());
 	}
 }
