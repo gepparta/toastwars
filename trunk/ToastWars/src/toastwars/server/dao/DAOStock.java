a57a198820a5b4d4290fddd309bacd472bfa42ed
tobiasmerx@gmx.de <tobiasmerx@gmx.de@19d0b848-b977-11dd-a067-9b1d96bc3259>
2009-02-13 04:08:50 +0000

    Autorenschaft hinzugefügt

--- trunk/ToastWars/src/toastwars/server/dao/DAOStock.java
+++ trunk/ToastWars/src/toastwars/server/dao/DAOStock.java
@@ -1,5 +1,7 @@
 package toastwars.server.dao;
-
+/*
+ * Author: Tobias Merx
+ * */
 import java.sql.Connection;
 import java.sql.ResultSet; //import java.sql.ResultSetMetaData;
 import java.sql.Statement;
